16cb9c039c31e8af4fe8078d241ba85a889da1f7
Emilio <emiliothomasking@gmail.com>
2022-06-20 15:10:41 -0700

    fixed menu trigger

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -119,48 +119,6 @@
 		}
 	});
 
-	$(document).ready(function () {
-	    $(document).on("scroll", onScroll);
-	    
-	    //smoothscroll
-	    $('.scroll-to-section a[href^="#"]').on('click', function (e) {
-			e.preventDefault();
-			$(document).off("scroll");
-			
-			$('.scroll-to-section a').each(function () {
-				$(this).removeClass('active');
-			})
-			$(this).addClass('active');
-		  
-			var target = this.hash,
-			menu = target;
-			var target = $(this.hash);
-			$('html, body').stop().animate({
-				scrollTop: (target.offset().top) - 79
-			}, 500, 'swing', function () {
-				window.location.hash = target;
-				$(document).on("scroll", onScroll);
-			});
-	        
-	    });
-	});
-
-	function onScroll(event){
-	    var scrollPos = $(document).scrollTop();
-	    $('.nav a').each(function () {
-	        var currLink = $(this);
-	        var refElement = $(currLink.attr("href"));
-	        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-	            $('.nav ul li a').removeClass("active");
-	            currLink.addClass("active");
-	        }
-	        else{
-	            currLink.removeClass("active");
-	        }
-	    });
-	}
-
-
 	// Page loading animation
 	$(window).on('load', function() {
 		if($('.cover').length){
