ed3814eb77e1c459605471084f605b029c9e2040
Emili0 <emiliothomasking@gmail.com>
2022-09-17 00:48:31 -0700

    changed coming soon

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 //typing animation
 var typed = new Typed(".typing",{
-    strings:["", "Coming ^400 Soon! ^4000", ""],
+    strings:["", "Coming ^400 Soon ^500... ^4000", ""],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
